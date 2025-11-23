608241c09a77c8bc5bcfbae70217ab86eb9b14f7
Mashal Shakeel <160768333+MashalShakeel@users.noreply.github.com>
2025-11-23 17:56:15 +0500

    Update embed.py

--- embed.py
+++ embed.py
@@ -1,8 +1,8 @@
 import torch
 from torchvision import models, transforms
 from PIL import Image
+import numpy as np
 
-# loading pretrained ResNet50 once
 _model = None
 _transform = None
 
@@ -24,10 +24,29 @@ def load_model():
     return _model, _transform
 
 
-def compute_embedding(img: Image.Image):
+def compute_resnet50_embedding(img: Image.Image):
     model, transform = load_model()
     tensor = transform(img).unsqueeze(0)
     with torch.no_grad():
         vec = model(tensor).squeeze().numpy()  # shape 2048
     return vec
 
+def compute_histogram_embedding(img: Image.Image, bins=16):
+    img = img.convert("RGB")
+    arr = np.array(img)
+
+    hist_r, _ = np.histogram(arr[:, :, 0], bins=bins, range=(0, 256))
+    hist_g, _ = np.histogram(arr[:, :, 1], bins=bins, range=(0, 256))
+    hist_b, _ = np.histogram(arr[:, :, 2], bins=bins, range=(0, 256))
+
+    hist = np.concatenate([hist_r, hist_g, hist_b]).astype("float32")
+    hist /= (np.linalg.norm(hist) + 1e-12)
+    return hist
+
+def compute_embedding(img: Image.Image, model_type="resnet50"):
+    if model_type == "resnet50":
+        return compute_resnet50_embedding(img)
+    elif model_type == "hist":
+        return compute_histogram_embedding(img)
+    else:
+        raise ValueError(f"Unknown model_type: {model_type}")
